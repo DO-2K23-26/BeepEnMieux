cdf3910b96c838e10a1c1c801ac9435cf24a69f9
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-03-05 18:25:20 +0100

    Add controller, module, schema and service user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), UserModule],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
