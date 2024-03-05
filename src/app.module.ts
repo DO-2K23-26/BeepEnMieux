f8c885bbb9b61d72bbedc400e26892b61c258367
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-03-05 18:33:28 +0100

    add module, service, strategy user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), UserModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'yourSecretKey', // Replace with your own secret key
+      signOptions: { expiresIn: '1h' }, // Token expiration time
+    }),
+    UserModule,
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtModule],
+})
+export class AuthModule {}
