ef35d6c96e701af35e8a5ee497be7cfdf1350702
Louis Labeyrie <labeyrielouis@gmail.com>
2024-03-07 18:08:31 +0100

    Refactor code for better readability and formatting

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,27 +4,22 @@ import { User } from '@prisma/client';
 import { Message } from '@prisma/client';
 import { Groupe } from '@prisma/client';
 
-
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(userData): Promise<User>  
-  {    
-    const result = await this.prisma.user.create({data: userData});
+  async createUser(userData): Promise<User> {
+    const result = await this.prisma.user.create({ data: userData });
     return result;
   }
 
-  async createMessage(messageData): Promise<Message>  
-  {    
-    const result = await this.prisma.message.create({data: messageData});
+  async createMessage(messageData): Promise<Message> {
+    const result = await this.prisma.message.create({ data: messageData });
     return result;
   }
 
-  async createGroupe(groupeData): Promise<Groupe>  
-  {    
-    const result = await this.prisma.groupe.create({data: groupeData});
+  async createGroupe(groupeData): Promise<Groupe> {
+    const result = await this.prisma.groupe.create({ data: groupeData });
     return result;
   }
-
 }
