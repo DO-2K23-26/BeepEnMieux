0a9b9b77c8ab5ea47ca07e31a106a6b3efe4d6fe
giada-de-martino <giada.de-martino@etu.umontpellier.fr>
2024-03-08 01:03:54 +0100

    finitions de l'installation de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma.service';
+import { MessageModule } from './message/message.module';
+import { GroupeModule } from './groupe/groupe.module';
+import { UsersController } from './users/users.controller';
+import { MessageController } from './message/message.controller';
+import { GroupeController } from './groupe/groupe.controller';
+import { UsersService } from './users/users.service';
+import { MessageService } from './message/message.service';
+import { GroupeService } from './groupe/groupe.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL),
     UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    GroupeModule,
+    MessageModule,
     AuthModule,
     ChatModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [
+    AppController,
+    UsersController,
+    MessageController,
+    GroupeController,
+  ],
+  providers: [
+    AppService,
+    PrismaService,
+    UsersService,
+    MessageService,
+    GroupeService,
+  ],
   exports: [PrismaService],
 })
 export class AppModule {}
