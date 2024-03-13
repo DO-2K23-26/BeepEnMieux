a8cbee784e519157de1da546cf8223d39708e47d
Louis Labeyrie <labeyrielouis@gmail.com>
2024-03-13 20:34:41 +0100

    Frère Jacques, Frère Jacques Dormez vous ? Dormez vous ? Sonnez les matines, Sonnez les matines, Ding ding dong Ding ding dong

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
-import { MessageModule } from './message/message.module';
-import { GroupeModule } from './groupe/groupe.module';
-import { UsersController } from './users/users.controller';
-import { MessageController } from './message/message.controller';
-import { GroupeController } from './groupe/groupe.controller';
-import { UsersService } from './users/users.service';
-import { MessageService } from './message/message.service';
-import { GroupeService } from './groupe/groupe.service';
+import { LoggerModule } from 'nestjs-pino';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { GroupeModule } from './groupe/groupe.module';
+import { MessageModule } from './message/message.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, GroupeModule, MessageModule, AuthModule, ChatModule, PrismaModule],
-  controllers: [
-    AppController,
-    UsersController,
-    MessageController,
-    GroupeController,
+  imports: [
+    UsersModule,
+    GroupeModule,
+    MessageModule,
+    AuthModule,
+    ChatModule,
+    PrismaModule,
+    LoggerModule.forRoot(),
   ],
-  providers: [
-    AppService,
-    PrismaService,
-    UsersService,
-    MessageService,
-    GroupeService,
-  ],
+  providers: [PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import * as dotenv from 'dotenv';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { bufferLogs: true },
+  );
+  app.useLogger(app.get(Logger));
   await app.listen(9644, '0.0.0.0');
 }
 bootstrap();
