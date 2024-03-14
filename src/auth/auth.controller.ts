0a5fa5cc186fa674f5e94a773fc2c2afd0f34d50
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-03-14 15:57:21 +0100

    modif auth controller for refresh

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,41 +1,45 @@
-import {
-  Controller,
-  Request,
-  Post,
-  UseGuards,
-  Body,
-  Get,
-  Headers,
-  NotAcceptableException,
-} from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body, Get, Headers, NotAcceptableException, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
-  @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.body);
-  }
+    @Post('login')
+    async login(@Request() req, @Res() res) {
+        const accessToken = await this.authService.login(req.body);
+        const refreshToken = await this.authService.refreshToken(req.body);
 
-  @UseGuards(AuthGuard('local'))
-  @Get('@me')
-  async getProfile(
-    @Headers('authorization') authorization: string,
-  ): Promise<any> {
-    if (!authorization) {
-      throw new NotAcceptableException('could not find the jwt token');
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'strict' });
+        return accessToken
     }
 
-    // Extrait le token du body "authorization" (format: "Bearer <token>")
-    const token = authorization.split(' ')[1];
+    @Post('refresh')
+    async refreshToken(@Request() req, @Body() body, @Res() res) {
+        const oldRefreshToken = req.cookies['refreshToken'];
+        const email = this.authService.verifyRefreshToken(oldRefreshToken);
+        const accessToken = await this.authService.login({ email });
+        const refreshToken = await this.authService.refreshToken({ email });
 
-    // Vérifie le token et récupère les informations de l'utilisateur
-    const userProfile = await this.authService.infoUser(token);
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'strict' });
+        return accessToken
+    }
 
-    // Utilisez les informations de l'utilisateur comme nécessaire
-    return userProfile;
-  }
+    @UseGuards(AuthGuard('local'))
+    @Get('@me')
+    async getProfile(@Headers('authorization') authorization: string): Promise<any> {
+        if (!authorization) {
+            throw new NotAcceptableException('could not find the jwt token');
+        }
+
+        // Extrait le token du body "authorization" (format: "Bearer <token>")
+        const token = authorization.split(' ')[1];
+
+        // Vérifie le token et récupère les informations de l'utilisateur
+        const userProfile = await this.authService.infoUser(token);
+
+        // Utilisez les informations de l'utilisateur comme nécessaire
+        return userProfile;
+    }
 }
