fb35cb053a86b599a8a40bc7f71642c1939248d1
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-05-01 00:29:07 +0200

    tututu

--- src/channel/channel.controller.ts
+++ src/channel/channel.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { ChannelService } from './channel.service';
+import { Channel, Message } from '@prisma/client';
 
 @Controller('channel')
 export class ChannelController {
-    
+    constructor (
+        private readonly channelService: ChannelService,
+    ) {}
+
+    @Get()
+    async findChannelsByServerId(@Req() request: Request) : Promise<Channel[]> {
+        return this.channelService.findChannelsByServerId(request['server'].id);
+    }
+
+    @Get(':id')
+    async findOne(@Req() request: Request) : Promise<Channel> {
+        return this.channelService.findOne(request['channel'].id);
+    }
+
+    @Get(':id/messages')
+    async findMessagesByChannelId(@Req() request: Request) : Promise<Message[]> {
+        return this.channelService.findMessagesByChannelId(request['channel'].id);
+    }
+
 }
