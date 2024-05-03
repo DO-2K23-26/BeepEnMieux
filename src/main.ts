5c20cdc10ce955353120232579d195c3d2360e4f
Louis Labeyrie <labeyrielouis@gmail.com>
2024-05-03 22:10:21 +0200

    chore: Update Chart.lock and Chart.yaml for beepenmieux helm chart

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { Logger } from 'nestjs-pino';
-dotenv.config();
-
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import * as dotenv from 'dotenv';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { env } from 'process';
 
