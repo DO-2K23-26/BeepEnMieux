48ddc215f3019a2461cc8a26c2a2cb2d0ed67b11
Louis Labeyrie <labeyrielouis@gmail.com>
2024-05-04 23:36:02 +0200

    Add env support

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { Public } from './app.service';
+import { AppService, Public } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Public()
   @Get()
   getHello(): { message: string; version: string } {
-    return { message: 'up', version: '0.1.0' };
+    return this.appService.getStatus();
   }
 }
