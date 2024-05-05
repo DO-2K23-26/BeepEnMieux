950bd15478bdc00b8aad83a2c9594f12b546d537
Louis Labeyrie <labeyrielouis@gmail.com>
2024-05-05 21:03:06 +0200

    implement swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { Logger } from 'nestjs-pino';
-import { AppModule } from './app.module';
 import { env } from 'process';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
@@ -21,6 +22,16 @@ async function bootstrap() {
     origin: env.FRONTEND_URL,
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
